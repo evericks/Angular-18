b5bb2901e717e08cbac9ce54c33bb311c41fbb50
Evericks <votantai4899@gmail.com>
2024-08-08 21:56:30 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent, ContactComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'Angular18';
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  name: string | null | 'Dcm' | 'Cmm' | number = null;
+
+  private _address: string = 'Vinhomes';
+
+  // Thang chay dau tien, truoc ca ngOnInit
+  // DI
+
+  // Khi no duoc sinh ra
+  ngOnInit(): void {
+    console.log('Tao da duoc sinh ra');
+    setTimeout(() => {
+      this.name = 5;
+    }, 0)
+  }
+
+  // Khi tao da render noi dung
+  ngAfterViewInit(): void {
+    console.warn('Tao dang render noi dung ra template');
+  }
+
+  constructor() {
+    console.log('Tao la constructor');
+  }
+
+  ngOnDestroy(): void {
+
+  }
 }
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'contact',
+    standalone: true,
+    templateUrl: './contact.component.html',
+    styleUrls: ['contact.component.css']
+})
+export class ContactComponent implements OnInit {
+    constructor() { }
+    name: string = 'asasdasd';
+    ngOnInit(): void { }
+}
